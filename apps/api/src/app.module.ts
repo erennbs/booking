65dac4a4c2eed28ff5e7aec5555ffc64becba70f
Eren <erenbas3453@gmail.com>
2026-03-22 18:01:41 +0300

    Add imports and cors to app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { DrizzleModule } from './db/drizzle.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DrizzleModule,
+    AuthModule,
+    BookingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
